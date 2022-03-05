ce87e3f2b603be4402f22fd092bc0f21d705c507
Dianwen Wang <wangdw2012@gmail.com>
2022-03-06 12:55:51 +1300

    add eslint import rule

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import resolvers from '@src/resolvers';
 import { ApolloServer } from 'apollo-server';
 
-import resolvers from './resolvers';
 import typeDefs from './type-defs';
 
 const server = new ApolloServer({ resolvers, typeDefs });
