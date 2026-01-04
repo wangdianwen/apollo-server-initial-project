b7cfaafd7cc7e2eacbf0663ce26dac61fb3e3344
Wang Dianwen <wangdw2012@gmail.com>
2026-01-04 20:16:31 +0000

    fix lint

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './type-defs';
 
 const server = new ApolloServer({ resolvers, typeDefs });
+// eslint-disable-next-line no-console
 server.listen().then(({ url }) => console.info(`Server ready at ${url}. `));
